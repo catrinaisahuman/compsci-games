46043fece15fb157125e4a3b424c2b87966e422b
catrinaisahuman <jimmydababby@gmail.com>
2019-05-31 11:32:51 -0700

    i probably did something

--- idleGame/idleGame.js
+++ idleGame/idleGame.js
@@ -7,6 +7,9 @@ autoTester = false
 upgradeLevel = 1000
 spinPrice = 50
 spinnerAct = false
+riskPrice = price/2
+riskAvl = false
+
 document.getElementById('wheel').style.display = "none"
 
 
@@ -28,8 +31,30 @@ function improve(){
 	upgradeLevel *= 2
 }
 
+function spin(){
+		start()
+		count -= spinPrice
+		spinPrice += Math.round(spinPrice/4)
+		spinning = true
+		timer = 0
+		length = Math.round(random(2, 5))
+		document.getElementById('result').innerHTML = 'result: '
+}
+
+function risk(){
+	count -= riskPrice
+	result = Math.floor(random(0, 9))
+	if(result == 0){
+		testers = 0
+	} else {
+		testers += Math.round(testers/10)
+	}
+}
+
 function update(){
 	tps = testers * testerRate
+	riskPrice = Math.floor(price/2)
+
 
 	if(testers < 0){
 		testers = 0
@@ -38,6 +63,19 @@ function update(){
 	document.getElementById("price").innerHTML = price + " tests" 
 	document.getElementById("improvePrice").innerHTML = upgradeLevel + " tests"
 
+	document.getElementById("riskPrice").innerHTML = riskPrice + " tests |"
+	document.getElementById("riskResult").innerHTML = "9/10 gain "+ Math.round(testers/10) +" testers, 1/10 lose all testers"
+
+	if(testers == 10){
+		document.getElementById("riskResult").innerHTML = "9/10 gain "+ Math.round(testers/10) +" tester, 1/10 lose all testers"
+	}
+
+	if(testers < 10 && riskAvl == false){
+		document.getElementById("riskDiv").style.display = "none"
+	} else {
+		document.getElementById("riskDiv").style.display = "block"
+		riskAvl = true
+	}
 
 	if(autoTester == true){
 		document.getElementById("tps").innerHTML = "tests per second: "+tps
@@ -47,6 +85,7 @@ function update(){
 	if(testers == 1){
 		document.getElementById("testerCount").innerHTML = testers+" tester"
 	}
+
 
 	if(count >= spinPrice){
 		document.getElementById('spin').disabled = false
@@ -70,6 +109,13 @@ function update(){
 		document.getElementById('tester').disabled = false
 	} else {
 		document.getElementById('tester').disabled = true
+	}
+
+
+	if(count >= riskPrice){
+		document.getElementById('risk').disabled = false
+	} else {
+		document.getElementById('risk').disabled = true
 	}
 }
 
--- idleGame/wheel.js
+++ idleGame/wheel.js
@@ -2,6 +2,17 @@ rotation = 0
 spinning = false
 timer = 0
 length = 0
+under50 = true
+
+t0 = 0
+t1 = 1
+t2 = 2
+t3 = 2
+t4 = 5
+t5 = 5
+t6 = 10
+t7 = 10
+
 function preload(){
 	spinner = loadImage("spinner.png")
 }
@@ -12,16 +23,15 @@ function start(){
 	background(220)
 	pg = createGraphics(300, 280)
 	pg.image(spinner, 0, 0, 300, 280)
-	pg.text('+0', 70, 90)
-	pg.text('+1', 120, 70)
-	pg.text('+2', 200, 70)
-	pg.text('-2', 230, 120)
-	pg.text('+5', 230, 200)
-	pg.text('-5', 170, 230)
-	pg.text('+10', 100, 230)
-	pg.text('-10', 70, 170)
+	pg.text('+'+t0, 70, 90)
+	pg.text('+'+t1, 120, 70)
+	pg.text('+'+t2, 200, 70)
+	pg.text('-'+t3, 230, 120)
+	pg.text('+'+t4, 230, 200)
+	pg.text('-'+t5, 170, 230)
+	pg.text('+'+t6, 100, 230)
+	pg.text('-'+t7, 70, 170)
 	document.getElementById('result').innerHTML = 'result: '
-
 }
 
 
@@ -33,8 +43,18 @@ function draw(){
 		rotate(rotation)
 		imageMode(CENTER)
 		image(pg, 0, 0)
-		rotation += 0.1
+		rotation += 0.1	
 	}
+
+	t1 = Math.round(map(testers, 0, 50, 1, 5))
+	t2 = Math.round(map(testers, 0, 50, 2, 10))
+	t3 = Math.round(map(testers, 0, 50, 2, 10))
+	t4 = Math.round(map(testers, 0, 50, 5, 15))
+	t5 = Math.round(map(testers, 0, 50, 5, 15))
+	t6 = Math.round(map(testers, 0, 50, 10, 20))
+	t7 = Math.round(map(testers, 0, 50, 10, 20))
+
+	document.getElementById('spinPrice').innerHTML = spinPrice + ' tests'
 
 	if(timer == length && spinning == true){
 		switch(Math.floor(random(1, 9))){
@@ -42,32 +62,32 @@ function draw(){
 				document.getElementById('result').innerHTML = 'result: +0'
 				break;
 			case 2:
-				testers += 1
-				document.getElementById('result').innerHTML = 'result: +1'
+				testers += t1
+				document.getElementById('result').innerHTML = 'result: +'+t1
 				break;
 			case 3:
-				testers += 2
-				document.getElementById('result').innerHTML = 'result: +2'
+				testers += t2
+				document.getElementById('result').innerHTML = 'result: +'+t2
 				break;
 			case 4:
-				testers -= 2
-				document.getElementById('result').innerHTML = 'result: -2'
+				testers -= t3
+				document.getElementById('result').innerHTML = 'result: -'+t3
 				break;
 			case 5:
-				testers += 5
-				document.getElementById('result').innerHTML = 'result: +5'
+				testers += t4
+				document.getElementById('result').innerHTML = 'result: +'+t4
 				break;
 			case 6:
-				testers -= 5
-				document.getElementById('result').innerHTML = 'result: -5'
+				testers -= t5
+				document.getElementById('result').innerHTML = 'result: -'+t5
 				break;
 			case 7:
-				testers += 10
-				document.getElementById('result').innerHTML = 'result: +10'
+				testers += t6
+				document.getElementById('result').innerHTML = 'result: +'+t6
 				break;
 			case 8:
-				testers -= 10
-				document.getElementById('result').innerHTML = 'result: -10'
+				testers -= t7
+				document.getElementById('result').innerHTML = 'result: -'+t7
 				break;
 		}	
 	}
@@ -78,13 +98,7 @@ function draw(){
 }
 
 
-function spin(){
-		count -= spinPrice
-		spinning = true
-		timer = 0
-		length = Math.round(random(2, 5))
-		document.getElementById('result').innerHTML = 'result: '
-}
+
 
 
 window.setInterval(function(){
